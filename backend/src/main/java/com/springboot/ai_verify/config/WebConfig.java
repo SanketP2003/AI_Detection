0fe45343fb40deacf57a6804263d55338c8ce3a5
SanketP2003 <1002sanketpatil@gmail.com>
2025-11-21 10:23:49 +0530

    Initial Commit

--- backend/src/main/java/com/springboot/ai_verify/config/WebConfig.java
+++ backend/src/main/java/com/springboot/ai_verify/config/WebConfig.java
@@ -1,11 +1,21 @@
 package com.springboot.ai_verify.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ai-detection-frontend.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
